d5d2f668c0ebc229bcb0122b89ec3323499d7828
Nick Espinosa <nickespi@gmail.com>
2013-05-06 18:13:25 -0500

    First commit.

---
+++ _assets/js/app-ck.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(window).load(function(){$(".slider").flexslider({slideshowSpeed:6e3,animationSpeed:600,controlNav:!0,directionNav:!0,useCSS:!1,animation:"slide",easing:"easeOutCirc"})})});
---
+++ _assets/js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function() { 
+			                
+			        
+
+  $(window).load(function() {
+    $('.slider').flexslider({
+    	slideshowSpeed: 6000,
+    	animationSpeed: 600,
+    	controlNav: true,  
+		directionNav: true,
+		useCSS: false,
+		animation: "slide",
+		easing: "easeOutCirc"
+    });
+
+  });
+
+
+}); 
